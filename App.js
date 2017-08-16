d3c22fc8d4b2719b1f865db15a8de9ad6fdc5ccd
William Kammersell <william.kammersell@ca.com>
2017-08-15 22:39:06 -0600

    Button to reset Team Board Quick Filters

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
+var app = null;
+
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+    
+    items: [
+		{
+			xtype: "rallybutton",
+			id: "reset-team-board-quick-filters-button",
+			text: "Reset Team Board Quick Filters",
+			handler: function() { app.resetTeamBoardQuickFilters(); },
+			margin: "5px"
+		},
+		{
+			xtype: "label",
+			id: "reset-team-board-quick-filters-label",
+			text: "",
+			margin: "5px"
+			
+		}
+    ],
+    
     launch: function() {
-        var bespoke_url = "https://rally1.rallydev.com/apps/bespoke/api/v1/board/current";
+		app = this;
+    },
+    
+    resetTeamBoardQuickFilters: function() {
+		app.down("#reset-team-board-quick-filters-button").setDisabled( true );
+		app.down("#reset-team-board-quick-filters-label").setText( "Processing..." );
         var xmlhttp = new XMLHttpRequest();
-        var response;
+        var callback = app.createNewBespoke;
         
         xmlhttp.onreadystatechange = function() {
-			if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
-				response = xmlhttp.responseXML;
+			if ( this.readyState == 4 && this.status == 200 ) {
+				callback( JSON.parse( this.responseText ) );
 			}
 		};
 		
-		//TODO - Figure out the supported way to delete Bespoke for the Team Board
-		xmlhttp.open( "DELETE", bespoke_url, false );
+		// Retrieve the current Bespoke for the Team Board
+		var bespoke_url = "https://rally1.rallydev.com/apps/bespoke/api/v1/board/current";
+		xmlhttp.open( "GET", bespoke_url, true );
 		xmlhttp.send();
+	},
+	
+	createNewBespoke: function( baseBespoke ) {
+		var bespokeId = baseBespoke.Id;
 		
-		console.log( response );
-    }
+		// Update the Bespoke for a send
+		baseBespoke.Filters.QuickFilters = [];
+		delete baseBespoke.CreatedAt;
+		delete baseBespoke.UpdatedAt;
+		delete baseBespoke.IsView;
+		
+		var xmlhttp = new XMLHttpRequest();
+		var createNewBespokeCallback = app.patchBespokeResponse;
+		
+		xmlhttp.onreadystatechange = function() {
+			if ( this.readyState == 4 && this.status == 200 ) {
+				createNewBespokeCallback( JSON.parse( this.responseText ) );
+			}
+		};
+		
+		// Update Bespoke
+		var bespoke_url = "https://rally1.rallydev.com/apps/bespoke/api/v1/board/" + bespokeId;
+		xmlhttp.open( "PATCH", bespoke_url, true );
+		xmlhttp.setRequestHeader( "content-type", "application/json" );
+		xmlhttp.send( JSON.stringify( baseBespoke ) );
+    },
+    
+    patchBespokeResponse: function( bespokePatchResponse ) {
+		app.down("#reset-team-board-quick-filters-button").setDisabled( false );
+		app.down('#reset-team-board-quick-filters-label').setText( "Done!" );
+	}
 });
