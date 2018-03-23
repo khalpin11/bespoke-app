27670d9518b3d8517682b0a2ea54549caa1d225c
Pairing <pairing@animal.ca.com>
2018-03-23 13:14:56 -0600

    add wiping of fields to original script

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ var app = null;
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    
+
     items: [
 		{
 			xtype: "rallybutton",
@@ -17,57 +17,60 @@ Ext.define('CustomApp', {
 			id: "reset-team-board-quick-filters-label",
 			text: "",
 			margin: "5px"
-			
+
 		}
     ],
-    
+
     launch: function() {
 		app = this;
     },
-    
+
     resetTeamBoardQuickFilters: function() {
 		app.down("#reset-team-board-quick-filters-button").setDisabled( true );
 		app.down("#reset-team-board-quick-filters-label").setText( "Processing..." );
         var xmlhttp = new XMLHttpRequest();
         var callback = app.createNewBespoke;
-        
+
         xmlhttp.onreadystatechange = function() {
 			if ( this.readyState == 4 && this.status == 200 ) {
 				callback( JSON.parse( this.responseText ) );
 			}
 		};
-		
+
 		// Retrieve the current Bespoke for the Team Board
 		var bespoke_url = "/apps/bespoke/api/v1/board/current";
 		xmlhttp.open( "GET", bespoke_url, true );
 		xmlhttp.send();
 	},
-	
+
 	createNewBespoke: function( baseBespoke ) {
 		var bespokeId = baseBespoke.Id;
-		
+
 		// Update the Bespoke for a send
 		baseBespoke.Filters.QuickFilters = [];
 		delete baseBespoke.CreatedAt;
 		delete baseBespoke.UpdatedAt;
 		delete baseBespoke.IsView;
-		
+
+    // clear fields
+    baseBespoke.Fields = [];
+
 		var xmlhttp = new XMLHttpRequest();
 		var createNewBespokeCallback = app.patchBespokeResponse;
-		
+
 		xmlhttp.onreadystatechange = function() {
 			if ( this.readyState == 4 && this.status == 200 ) {
 				createNewBespokeCallback( JSON.parse( this.responseText ) );
 			}
 		};
-		
+
 		// Update Bespoke
 		var bespoke_url = "/apps/bespoke/api/v1/board/" + bespokeId;
 		xmlhttp.open( "PATCH", bespoke_url, true );
 		xmlhttp.setRequestHeader( "content-type", "application/json" );
 		xmlhttp.send( JSON.stringify( baseBespoke ) );
     },
-    
+
     patchBespokeResponse: function( bespokePatchResponse ) {
 		app.down("#reset-team-board-quick-filters-button").setDisabled( false );
 		app.down('#reset-team-board-quick-filters-label').setText( "Done!" );
